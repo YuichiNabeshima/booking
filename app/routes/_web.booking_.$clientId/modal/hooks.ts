e033f2b8ec5bd524951a331409cc74323adb7914
YuichiNabeshima <denmukuru1208@gmail.com>
2024-07-23 01:12:22 -0700

    feat: :sparkles: #4 Convert the url in mail body to in the anchor tag

--- app/routes/_web.booking_.$clientId/modal/hooks.ts
+++ app/routes/_web.booking_.$clientId/modal/hooks.ts
@@ -42,8 +42,20 @@ export function useModal({ result }: Args) {
 
   }, [result]);
 
+  /**
+   * Convert the '\n' to <br>.
+   */
   function convertToBr(text: string) {
     return { __html: text.replace('\n', '<br>') ?? '' };
+  }
+
+  /**
+   * Convert url in email body to in the anchor tag.
+   */
+  function convertToAnchor(text: string) {
+    const link = text.match(/https?:\/\/.*\n/);
+    const linkRemoveBr = link?.[0].replace(/\n$/, '');
+    return text.replace(/https?:\/\/.*\n/, `<a href="${linkRemoveBr}" target="_blank">${linkRemoveBr}</a>\n`);
   }
 
   return {
@@ -51,5 +63,6 @@ export function useModal({ result }: Args) {
     isConfirm,
     isFinish,
     convertToBr,
+    convertToAnchor,
   };
 }
